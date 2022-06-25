dd8a49ffed88faaaa74d5317e7541dd18c7041f6
Maykesh <Maykeshmaygi1996@gmail.com>
2022-06-25 21:26:41 +0530

    message

---
+++ src/main/java/org/GIT/Repositry/App.java
@@ -0,0 +1,13 @@
+package org.GIT.Repositry;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/comp/Repositry.java
@@ -0,0 +1,19 @@
+package org.comp;
+
+public class Repositry {
+
+	public void hardwareResources() {
+		
+System.out.println("23gb");
+	}
+	
+	public void softwareResources() {
+		System.out.println("32gb");
+	}
+	
+	public void desktopModel() {
+		
+System.out.println("56gb");
+	}
+	
+}
