c99c603284875b60bd475089b228f818fad096ae
Maykesh <Maykeshmaygi1996@gmail.com>
2022-11-12 18:47:25 +0530

    adding 3 more methods

--- src/test/java/org/comp/Repositry.java
+++ src/test/java/org/comp/Repositry.java
@@ -3,17 +3,31 @@ package org.comp;
 public class Repositry {
 
 	public void hardwareResources() {
-		
-System.out.println("23gb");
+
+		System.out.println("23gb");
 	}
-	
+
 	public void softwareResources() {
 		System.out.println("32gb");
 	}
-	
+
 	public void desktopModel() {
-		
-System.out.println("56gb");
+
+		System.out.println("56gb");
 	}
-	
+
+	public void cycleModel() {
+
+		System.out.println("cycle");
+	}
+
+	public void bikeModel() {
+
+		System.out.println("bike");
+	}
+
+	public void carModel() {
+
+		System.out.println("car");
+	}
 }
